94d3e523ef064e04bca8b8e55e2b66b05e6ebcf7
神山仁丹 <jin81928192@gmail.com>
2019-10-12 23:11:58 +0900

    URLを書いて直接詳細画面などに入ることを防ぐためにコンストラクタを定義

--- app/Http/Controllers/TweetController.php
+++ app/Http/Controllers/TweetController.php
@@ -10,6 +10,12 @@ use App\Tweet;
 
 class TweetController extends Controller
 {
+    public function __construct()
+{
+	$this->middleware('auth', [
+		'only' => ['create','store','edit','update','destroy']
+	]);
+}    
     /**
      * Display a listing of the resource.
      *
