0bc494b3b296db5c9d520b2f42b3239e38890009
神山仁丹 <jin81928192@gmail.com>
2019-10-13 16:22:40 +0900

    ユーザーが新規登録された時にuser_profileも作成する様に記述（要復習）

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\User;
+use App\UserProfile;
 use Validator;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\ThrottlesLogins;
@@ -35,7 +36,7 @@ class AuthController extends Controller
         // routes.phpでこのAuthControllerが呼ばれたらconstructだからこのメソッドが絶対呼ばれる
         //ログイン、ログアウト、新規登録した後にどの画面に飛ぶかを書いている
         //redirectPathというプロパティにtweets.indexというツイート一覧画面のURLを渡してあげて、ユーザーが作成されたあとはツイート一覧画面へリダレクト
-        $this->redirectPath = route('tweets.index');
+        $this->redirectPath = route('tweets.index');        
         //ログインされたあとはツイート一覧画面へリダイレクト
         $this->loginPath = route('tweets.index');
         //ログアウトしたあとはログイン画面へリダイレクト
@@ -66,10 +67,28 @@ class AuthController extends Controller
      */
     protected function create(array $data)
     {
-        return User::create([
+        //returnだとそこで処理が終わってしまってuserprofilesの保存をする隙間がないから$userに変更
+        $user =  User::create([
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
         ]);
+
+        //user_profilesの保存
+        $user_profile = new UserProfile();
+        $user_profile->introduction = "こんにちは！{ $user->name }です！";
+        $user_profile->birthday = '1990-01-01';
+        //普通は
+        //$user_profile->user_id = $user->id;
+        //$user_profile->save();
+        //こうするけど今回は、
+        $user->userProfile()->save($user_profile);
+        //userprofile()はUse.phpというモデルに定義されているメソッド
+        //save($user_profile)の$user_profileはこの上でnewしたやつのこと
+        //この処理を行うと上の$user->~~~の$userに関連したレコードとしてsave->($user_profile)の$user_profileが保存される
+        //！！！！！！要復習！！！！！！
+
+        return $user;
+
     }
 }
