b2071c8a8624e8f560a4b4d87d63896c335747eb
神山仁丹 <jin81928192@gmail.com>
2019-10-13 13:36:38 +0900

    ユーザーとユーザープロフィールのモデルをEloquentのサポートを受けられる様にメソッドを定義

--- app/User.php
+++ app/User.php
@@ -41,4 +41,9 @@ class User extends Model implements AuthenticatableContract,
     {
         return $this->hasMany('App\Tweet');
     }
+
+    public function userProfile()
+    {
+        return $this->hasOne('App\UserProfile');
+    }
 }
